97d169630bd9afa35980f02f412f99199c38c5eb
kitanikit <nikitasentyurin@gmail.com>
2024-03-05 16:51:05 +0300

    исправлен input

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleClearIcon(inputElement) {
-  const clearIcon = inputElement.nextSibling;
-  if (inputElement.value.length > 0) {
-    clearIcon.style.display = 'block';
-  } else {
-    clearIcon.style.display = 'none';
-  }
-}
